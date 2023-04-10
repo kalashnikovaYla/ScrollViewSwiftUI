2c3fc0e04dfe400e428484bf36ab27dada57b2ee
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-10 21:06:55 +0500

    Initial Commit

---
+++ ScrollViewSwiftUI/ScrollViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewSwiftUIApp.swift
+//  ScrollViewSwiftUI
+//
+//  Created by sss on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
